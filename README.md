baae87957889f282014b051a591a006792c5a29f
Jonathan Jade Uyanguren <jade.uyanguren@inriver.com>
2024-01-14 02:26:30 +0800

    Partial code drop

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vuetodolist
 Develop a to-do list web application similar to what can be seen in the given web application.
+sample web app - https://drive.google.com/file/d/14yVb9gVXmp1upRkrDFSIO3Jf15we4rwk/view?usp=sharing
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main class="main-wrapper">
+      <TodoList/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList/TodoList';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.main-wrapper {
+  background-color: rgb(134, 226, 233);
 }
 </style>
